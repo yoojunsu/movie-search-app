2203cc3f2016030d9a6bff601e9f55b239034943
yoojunsu <vcoavmv@naver.com>
2023-10-19 00:04:39 +0900

    [영화검색앱] 공통 요소 스타일 css 추가

--- src/App.vue
+++ src/App.vue
@@ -8,30 +8,29 @@
 </template>
 
 <script>
-import { mapState, mapActions } from 'vuex';
+import { mapState, mapActions } from "vuex";
 // 컴포넌트 import
 import SearchBar from "./components/SearchBar.vue";
 import Loading from "./components/Loading.vue";
 export default {
-  name: 'App',
+  name: "App",
   components: {
     SearchBar,
     Loading,
   },
   computed: {
-    ...mapState('MovieModule',['IsLoading']),
+    ...mapState("MovieModule", ["IsLoading"]),
   },
   methods: {
-    ...mapActions('MovieModule',['fetchSearchMovie']),
+    ...mapActions("MovieModule", ["fetchSearchMovie"]),
   },
   mounted() {
     this.fetchSearchMovie();
-  }
-}
+  },
+};
 </script>
 
 <style>
-
 * {
   margin: 0;
   padding: 0;
@@ -56,16 +55,25 @@ section {
   margin: 0 auto;
 }
 
-button,input,select {
+a,
+button {
+  color: #000;
+}
+
+button,
+input,
+select {
   box-sizing: border-box;
   border: 0;
 }
 
-input,button {
+input,
+button {
   box-sizing: border-box;
 }
 
-input,select {
+input,
+select {
   background-color: #fff;
   outline: none;
 }
@@ -74,10 +82,22 @@ main {
   padding-top: 69px;
 }
 
-
 button {
   background-color: transparent;
   cursor: pointer;
 }
 
+.content-box {
+  overflow: hidden;
+}
+
+.scr-box {
+  padding: 40px 0 25px;
+  overflow-x: auto;
+}
+
+.card-list {
+  display: flex;
+  gap: 0 25px;
+}
 </style>
