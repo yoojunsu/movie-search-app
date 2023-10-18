48c7f03e86d587403597ed2e6c5220d5a3fde0ed
yoojunsu <vcoavmv@naver.com>
2023-10-19 00:08:34 +0900

    [영화검색앱] 메인 컨텐츠 작업
    1. 'NowPlayingMovie' 컨텐츠 추가 작업
    2. 'TopRatedMovie' 컨텐츠 추가작업 ~ing
    3. 'TopRatedMovie' 장르 변경기능 추가 예정 (State, Mutations 항목생성완료 및 메인컴포넌트에  장르 감시자 watcher항목 생성)

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,139 +1,314 @@
 <template>
-    <!-- 검색 영역 -->
-    <section class="main-search-section" :style="{backgroundImage: `url('${MainRandomVisualBg}')`}">
-        <div class="section-inner">
-            <div class="tit-wrap">
-                <h2>WELCOME</h2>
-                <h3>Search for movies you want</h3>
-            </div>
-            <div class="search-wrap">
-                <input type="text" name="" id="" placeholder="영화,TV 프로그램 등 검색..."/>
-                <button type="button" id="searchBtn">
-                    <i class="xi-search"></i>
-                </button>
-            </div>
-        </div>
-    </section>
-    <!-- ...검색영역 -->
+  <!-- 검색 영역 -->
+  <section
+    class="main-search-section"
+    :style="{ backgroundImage: `url('${MainRandomVisualBg}')` }"
+  >
+    <div class="section-inner">
+      <div class="tit-wrap">
+        <h2>WELCOME</h2>
+        <h3>Search for movies you want</h3>
+      </div>
+      <div class="search-wrap">
+        <input
+          type="text"
+          name=""
+          id=""
+          placeholder="영화,TV 프로그램 등 검색..."
+        />
+        <button type="button" id="searchBtn">
+          <i class="xi-search"></i>
+        </button>
+      </div>
+    </div>
+  </section>
+  <!-- ...검색영역 -->
 
-    <!-- 최신상영작 리스트 -->
-    <section class="content-section nowplaying-section">
-        <div class="section-inner">
-            <h3 class="section-tit">Now Playing!</h3>
-            <div class="scr-box">
-                <div v-for="(nowPlayingMovie,i) in NowPlayingMovies" :key="i">
-                    <a :href="`https://www.youtube.com/watch?v=${nowPlayingMovie}`">
-                        {{ nowPlayingMovie.original_title }}
-                        
-                    </a>
+  <!-- 최신상영작 리스트 -->
+  <section class="content-section nowplaying-section">
+    <div class="section-inner">
+      <h3 class="section-tit">Now Playing!</h3>
+      <div class="content-box">
+        <div class="scr-box">
+          <ul class="card-list">
+            <li
+              class="card"
+              v-for="(nowPlayingMovie, i) in NowPlayingMovies"
+              :key="i"
+            >
+              <div class="poster-wrap">
+                <img
+                  :src="`https://www.themoviedb.org/t/p/w440_and_h660_face${nowPlayingMovie.poster_path}`"
+                  alt=""
+                />
+
+                <div class="vote-wrap">
+                  <b>
+                    {{ nowPlayingMovie.vote_average }}
+                  </b>
                 </div>
-                <ul class="nowplaying-list"></ul>
-            </div>
+              </div>
+              <div class="info-wrap">
+                <h4>
+                  {{ nowPlayingMovie.title }}
+                </h4>
+                <span>
+                  {{ nowPlayingMovie.release_date }}
+                </span>
+              </div>
+            </li>
+          </ul>
         </div>
-    </section>
-    <!-- ...최신 상영작 리스트 -->
+      </div>
+    </div>
+  </section>
+  <!-- ...최신 상영작 리스트 -->
+
+  <!-- 최고점수 영화 리스트 -->
+  <section class="content-section nowplaying-section">
+    <div class="section-inner">
+      <h3 class="section-tit">What's Popular</h3>
+      <div class="btn-wrap">
+        <button type="button" :class="{ active: Genre }">MOVIE</button>
+        <button type="button" :class="{ active: Genre === false }">TV</button>
+      </div>
+      <div class="content-box">
+        <div class="scr-box">
+          <ul class="card-list">
+            <li
+              class="card"
+              v-for="(topRatedMovie, i) in TopRatedMovies"
+              :key="i"
+            >
+              <div class="poster-wrap">
+                <img
+                  :src="`https://www.themoviedb.org/t/p/w440_and_h660_face${topRatedMovie.poster_path}`"
+                  alt=""
+                />
+
+                <div class="vote-wrap">
+                  <b>
+                    {{ topRatedMovie.vote_average }}
+                  </b>
+                </div>
+              </div>
+              <div class="info-wrap">
+                <h4>
+                  {{ topRatedMovie.title }}
+                </h4>
+                <span>
+                  {{ topRatedMovie.release_date }}
+                </span>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- ...최고점수영화 리스트 -->
+  <div class="content-section"></div>
 </template>
 
 <script>
-import { mapActions, mapMutations, mapState } from 'vuex'
+import { mapActions, mapMutations, mapState } from "vuex";
 export default {
-    name: "Main",
-    computed: {
-        ...mapState('MovieModule',['NowPlayingMovies','PopularMovies','TopRatedMovies', 'MainRandomVisualBg']),
+  name: "Main",
+  computed: {
+    ...mapState("MovieModule", [
+      "NowPlayingMovies",
+      "PopularMovies",
+      "TopRatedMovies",
+      "MainRandomVisualBg",
+      "Genre",
+    ]),
+  },
+  watch: {
+    // Genre State값 변경 감시 watcher
+    Genre(genre) {
+      genre === "movie" ? true : false;
     },
-    methods: {
-        ...mapMutations('MovieModule',['setMainVisualRandomBg']),
-        ...mapActions('MovieModule',['fetchMainMovies']),
+  },
+  methods: {
+    ...mapMutations("MovieModule", ["setMainVisualRandomBg"]),
+    ...mapActions("MovieModule", ["fetchMainMovies"]),
 
-        //메인 최상단 비주얼 bg 영화 backdrop random 출력
-        randomVisualBg() {
-            const mainRandomVisualImagePath = this.PopularMovies[Math.floor(Math.random() * this.PopularMovies.length)].backdrop_path;
-            const mainRandomVisualImageOptions = 'w1920_and_h600_multi_faces_filter(duotone,000,00baff)';
-            const mainRandomVisualImageUrl = `https://image.tmdb.org/t/p/${mainRandomVisualImageOptions}${mainRandomVisualImagePath}`;
+    //메인 최상단 비주얼 bg 영화 backdrop random 출력
+    randomVisualBg() {
+      const mainRandomVisualImagePath =
+        this.PopularMovies[
+          Math.floor(Math.random() * this.PopularMovies.length)
+        ].backdrop_path;
+      const mainRandomVisualImageOptions =
+        "w1920_and_h600_multi_faces_filter(duotone,000,00baff)";
+      const mainRandomVisualImageUrl = `https://image.tmdb.org/t/p/${mainRandomVisualImageOptions}${mainRandomVisualImagePath}`;
 
-            this.setMainVisualRandomBg(mainRandomVisualImageUrl)
-        }
+      this.setMainVisualRandomBg(mainRandomVisualImageUrl);
     },
+  },
 
-    async created() {
-        //Main 컴포넌트 컨텐츠 세팅 데이터 fetch
-        await this.fetchMainMovies();
+  async created() {
+    //Main 컴포넌트 컨텐츠 세팅 데이터 fetch
+    await this.fetchMainMovies();
 
-        this.randomVisualBg();
-    }, 
-}
+    this.randomVisualBg();
+  },
+};
 </script>
 
 <style scoped lang="scss">
 .main-search-section {
-    background-color: #00baff;
-    background-size: cover;
-    background-position: center;
-    max-width: 1300px;
-    height: 350px;
-    margin: 0 auto;
-    transition: background-image 0.3s;
+  background-color: #00baff;
+  background-size: cover;
+  background-position: center;
+  max-width: 1300px;
+  height: 350px;
+  margin: 0 auto;
+  transition: background-image 0.3s;
 
-    .section-inner {
-        box-sizing: border-box;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        background-color: rgba(0, 0, 0, 0.3);
-        width: 100%;
-        height: 100%;
-        padding: 0 25px;
+  .section-inner {
+    box-sizing: border-box;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    background-color: rgba(0, 0, 0, 0.3);
+    width: 100%;
+    height: 100%;
+    padding: 0 25px;
+  }
+
+  h2,
+  h3 {
+    color: #fff;
+  }
+
+  .tit-wrap {
+    margin-bottom: 30px;
+
+    h2 {
+      font-size: 4rem;
     }
 
-    h2,h3 {
-        color: #fff;
+    h3 {
+      font-size: 3rem;
+    }
+  }
+
+  .search-wrap {
+    border-radius: 50px;
+    box-shadow: 0 7px 8px rgba(0, 0, 0, 0.4);
+    width: 100%;
+    position: relative;
+    overflow: hidden;
+
+    input {
+      padding: 15px 125px 15px 25px;
+      width: 100%;
     }
 
-    .tit-wrap {
-        margin-bottom: 30px;
+    button {
+      background: linear-gradient(
+        90deg,
+        rgba(255, 242, 0, 1) 0%,
+        rgba(255, 255, 255, 1) 87%
+      );
+      position: absolute;
+      top: 0;
+      right: 0;
+      width: 100%;
+      height: 100%;
+      max-width: 100px;
 
-        h2 {
-            font-size: 4rem;
-        }
-
-        h3 {
-            font-size: 3rem;
-        }
-
+      i {
+        font-size: 2.3rem;
+      }
     }
-
-    .search-wrap {
-        border-radius: 50px;
-        box-shadow: 0 7px 8px rgba(0, 0, 0, 0.4);
-        width: 100%;
-        position: relative;
-        overflow: hidden;
-
-        input {
-            padding: 15px 125px 15PX 25PX;
-            width: 100%;
-        }
-
-        button {
-            background: linear-gradient(90deg, rgba(255,242,0,1) 0%, rgba(255,255,255,1) 87%);
-            position: absolute;
-            top: 0;
-            right: 0;
-            width: 100%;
-            height: 100%;
-            max-width: 100px;
-
-            i {font-size: 2.3rem}
-        }
-    }
+  }
 }
 
 .content-section {
-    padding: 60px 0;
+  padding: 60px 0;
+
+  .btn-wrap {
+    display: flex;
+    border: 1px solid #000;
+    border-radius: 50px;
+    max-width: 230px;
+    overflow: hidden;
+
+    button {
+      font-size: 1.5rem;
+      width: 100%;
+      padding: 15px 0;
+      text-align: center;
+
+      &.active {
+        background-color: #00baff;
+        color: #fff;
+        font-weight: 600;
+      }
+    }
+  }
 }
 
 .section-tit {
-    font-size: 4rem; color: #000;
-    margin-bottom: 30px;    
+  font-size: 4rem;
+  color: #000;
+  margin-bottom: 30px;
+}
+
+.card {
+  min-width: 200px;
+  text-align: center;
+
+  .info-wrap {
+    h4 {
+      font-size: 1.8rem;
+      letter-spacing: -0.05em;
+      margin: 20px 0 10px;
+    }
+
+    span {
+      font-size: 1.6rem;
+      color: #ccc;
+    }
+  }
+
+  .vote-wrap {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background: #000;
+    border: 2px solid #00baff;
+    border-radius: 50%;
+    width: 35px;
+    height: 35px;
+    position: absolute;
+    top: -15px;
+    right: -10px;
+
+    b {
+      color: #fff;
+      font-size: 1.3rem;
+    }
+  }
+}
+
+.poster-wrap {
+  width: 100%;
+  height: 0;
+  padding-bottom: calc(270 / 200 * 100%);
+  position: relative;
+
+  img {
+    border-radius: 20px;
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    top: 0;
+    left: 0;
+    object-fit: cover;
+    object-position: center;
+  }
 }
 </style>
--- src/store/modules/MovieModule.js
+++ src/store/modules/MovieModule.js
@@ -1,98 +1,117 @@
 import axios from "axios";
 
 const key = process.env.VUE_APP_API_KEY;
-const apiUrl = 'https://api.themoviedb.org/3/movie/';
+const apiUrl = "https://api.themoviedb.org/3";
 const state = {
-    PopularMovies: [],
-    TopRatedMovies: [],
-    UpcomingMovies: [],
-    NowPlayingMovies: [],
-    MainRandomVisualBg: "",
-    SearchMovies: [],
-    MovieId: "",
-    SearchQuery: "슬램덩크",
-    IsLoading: false,
-}
+  Genre: "movie",
+  PopularMovies: [],
+  TopRatedMovies: [],
+  UpcomingMovies: [],
+  NowPlayingMovies: [],
+  MainRandomVisualBg: "",
+  SearchMovies: [],
+  MovieId: "",
+  SearchQuery: "슬램덩크",
+  IsLoading: false,
+};
 
 const mutations = {
-    //메인화면 Movies 셋팅
-    setMainMovies(state, {popularData, topRatedData, upcomingData, nowPlayingData}) {
-        state.PopularMovies = popularData;
-        state.TopRatedMovies = topRatedData;
-        state.UpcomingMovies = upcomingData;
-        state.NowPlayingMovies = nowPlayingData;
-    },
+  // 최초 장르 셋팅
+  setGenre(state, payload) {
+    state.Genre = payload;
+  },
+  //메인화면 Movies 셋팅
+  setMainMovies(
+    state,
+    { popularData, topRatedData, upcomingData, nowPlayingData }
+  ) {
+    state.PopularMovies = popularData;
+    state.TopRatedMovies = topRatedData;
+    state.UpcomingMovies = upcomingData;
+    state.NowPlayingMovies = nowPlayingData;
+  },
 
-    //메인화면 visual randomBg state셋팅
-    setMainVisualRandomBg(state,randomBgData) {
-        state.MainRandomVisualBg = randomBgData;
-    },
+  //메인화면 visual randomBg state셋팅
+  setMainVisualRandomBg(state, randomBgData) {
+    state.MainRandomVisualBg = randomBgData;
+  },
 
-    //검색 Movies 셋팅
-    setSearchMovies(state, SearchMovieData) {
-        state.SearchMovies = SearchMovieData;
-    }
+  //검색 Movies 셋팅
+  setSearchMovies(state, SearchMovieData) {
+    state.SearchMovies = SearchMovieData;
+  },
 };
 
 const actions = {
+  //Main화면에 최초 노출될 Movie 데이터 Main화면 컨텐츠 state에 기입
+  async fetchMainMovies({ commit }) {
+    try {
+      const popularResponce = await axios.get(`${apiUrl}/movie/popular`, {
+        params: {
+          api_key: key,
+          page: "1",
+        },
+      });
+      const topRatedResponce = await axios.get(`${apiUrl}/movie/top_rated`, {
+        params: {
+          api_key: key,
+          page: "1",
+        },
+      });
+      const upcomingResponce = await axios.get(`${apiUrl}/movie/upcoming`, {
+        params: {
+          api_key: key,
+          page: "1",
+        },
+      });
 
-    //Main화면에 노출될 Movie 데이터 Main화면 컨텐츠 state에 기입
-    async fetchMainMovies({commit}) {
-        try {
-            const popularResponce = await axios.get(`${apiUrl}/popular`,{
-                params: {
-                    api_key: key,
-                }
-            });
-            const topRatedResponce = await axios.get(`${apiUrl}/top_rated`, {
-                params: {
-                    api_key: key,
-                }
-            });
-            const upcomingResponce = await axios.get(`${apiUrl}/upcoming`, {
-                params: {
-                    api_key: key,
-                }
-            });
-
-            const nowPlayingResponce = await axios.get(`${apiUrl}/now_playing`,{
-                params: {
-                    api_key: key,
-                }
-            });
-
-
-            commit('setMainMovies',{
-                popularData: popularResponce.data.results,
-                topRatedData: topRatedResponce.data.results,
-                upcomingData: upcomingResponce.data.results,
-                nowPlayingData: nowPlayingResponce.data.results,
-            });
-
-        } catch (err) {
-            console.log(err);
+      const nowPlayingResponce = await axios.get(
+        `${apiUrl}/movie/now_playing`,
+        {
+          params: {
+            api_key: key,
+            page: "1",
+          },
         }
-    },
-    //SearchMovie 데이터 SearchMovies state에 기입
-    async fetchSearchMovie({ state,commit }) {
-        try {
-            const responce = await axios.get('https://api.themoviedb.org/3/search/movie',{
-                params: {
-                    api_key: key,
-                    query: state.SearchQuery
-                }
-            });
+      );
 
-            commit('setSearchMovies',responce.data.results);
-        } catch(err) {
-            console.log(err);
-        }
+      commit("setMainMovies", {
+        popularData: popularResponce.data.results,
+        topRatedData: topRatedResponce.data.results,
+        upcomingData: upcomingResponce.data.results,
+        nowPlayingData: nowPlayingResponce.data.results,
+      });
+      console.log(popularResponce.data.results);
+    } catch (err) {
+      console.log(err);
     }
-}
+  },
+
+  //Main 화면 장르변경 후 API 통신 actions - 추가작업 시작점
+  async fetchGenreUpdate() {},
+  //SearchMovie 데이터 SearchMovies state에 기입
+  async fetchSearchMovie({ state, commit }) {
+    try {
+      const responce = await axios.get(
+        "https://api.themoviedb.org/3/search/movie",
+        {
+          params: {
+            api_key: key,
+            query: state.SearchQuery,
+          },
+        }
+      );
+
+      commit("setSearchMovies", responce.data.results);
+    } catch (err) {
+      console.log(err);
+    }
+  },
+};
 
 export default {
-    namespaced: true,
-    state,
-    mutations,
-    actions,
-}
+  namespaced: true,
+  state,
+  mutations,
+  actions,
+};
